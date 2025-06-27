299d58ee1d1c830f1590953dff726aa4656e7304
Kishor Bhagat <kishorebhagat663@gmai.com>
2025-06-27 15:55:23 +0530

    feat: set up oracle jdbc connection

--- backend/src/main/java/org/example/onlineBankingSystem/app/OnlineBankingApp.java
+++ backend/src/main/java/org/example/onlineBankingSystem/app/OnlineBankingApp.java
@@ -1,6 +1,16 @@
 package org.example.onlineBankingSystem.app;
 
+import org.example.onlineBankingSystem.config.DBConfig;
+
+import java.sql.Connection;
+
 public class OnlineBankingApp {
     public static void main(String[] args) {
+        try {
+            Connection connection = DBConfig.getConnection();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
